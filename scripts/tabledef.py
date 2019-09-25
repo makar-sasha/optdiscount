b5051af6bc8a9b81ae8cdc446a04e5e4953e895a
Alex Makarenko <alex@frontsystems.no>
2019-09-25 23:10:28 +0200

    init 3

--- scripts/tabledef.py
+++ scripts/tabledef.py
@@ -20,7 +20,7 @@ def db_connect():
     Performs database connection using database settings from settings.py.
     Returns sqlalchemy engine instance
     """
-    return create_engine(SQLALCHEMY_DATABASE_URI, isolation_level='READ UNCOMMITTED')
+    return create_engine(SQLALCHEMY_DATABASE_URI, timeout=20)
 
 
 class User(Base):
